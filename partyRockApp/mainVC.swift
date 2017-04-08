814166c261c0b7c2ecf21ebb5de84044a280e685
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-09 01:32:24 +0300

    added an identifier in the main storyboard for the view that was missing it, moved some code around

--- partyRockApp/mainVC.swift
+++ partyRockApp/mainVC.swift
@@ -16,16 +16,22 @@ class mainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         let p1 = partyRock(imageURL: "https://parade.com/wp-content/uploads/2014/01/1-12-14-Neil-deGrasse-Tyson-inside-main-ftr.jpg", videoURL: "https://www.youtube.com/embed/n5uGrJvMieo", videoTitle: "Neil")
         partyRocks.append(p1)
         
+
+        
 //        let urlTest = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/n5uGrJvMieo\" frameborder=\"0\" allowfullscreen></iframe>"
-        tableView.delegate = self
-        tableView.dataSource = self
+
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "videoVCIden") as? partyCellTableViewCell{
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "partyCell_ID") as? partyCellTableViewCell{
             let partyRock = partyRocks[indexPath.row]
             cell.updateUI(partyRock: partyRock)
             return cell
