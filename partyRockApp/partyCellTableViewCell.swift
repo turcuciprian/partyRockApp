8b95c5c2a20bcf0d5b50bb05338cb0ab24c9137f
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-08 19:13:09 +0300

    created a party cell class file

---
+++ partyRockApp/partyCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  partyCellTableViewCell.swift
+//  partyRockApp
+//
+//  Created by Turcu Ciprian on 08/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class partyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func updateUI(){
+         
+    }
+
+}
