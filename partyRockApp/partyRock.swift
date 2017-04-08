0708ab98d57f2b6baf319c3bda4c0605fa54f12c
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-08 22:31:44 +0300

    setters variables for cell class

--- partyRockApp/partyRock.swift
+++ partyRockApp/partyRock.swift
@@ -12,7 +12,15 @@ class partyRock{
     private var _imageURL: String!
     private var _videoURL: String!
     private var _videoTitle: String!
-    
+    var imageURL: String{
+        return _imageURL
+    }
+    var videoURL: String{
+        return _videoURL
+    }
+    var videoTitle:String{
+        return _videoTitle
+    }
     init(imageURL:String, videoURL:String,videoTitle: String){
         _imageURL = imageURL
         _videoURL = videoURL
