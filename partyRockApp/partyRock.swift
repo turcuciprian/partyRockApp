b5568b5bd6a7e7f769c7614569670ee7f08a2e15
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-08 22:03:33 +0300

    created a new class with some variables

---
+++ partyRockApp/partyRock.swift
@@ -0,0 +1,22 @@
+//
+//  partyRock.swift
+//  partyRockApp
+//
+//  Created by Turcu Ciprian on 08/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import Foundation
+
+class partyRock{
+    private var _imageURL: String!
+    private var _videoURL: String!
+    private var _videoTitle: String!
+    
+    init(imageURL:String, videoURL:String,videoTitle: String){
+        _imageURL = imageURL
+        _videoURL = videoURL
+        _videoTitle = videoTitle
+        
+    }
+}
